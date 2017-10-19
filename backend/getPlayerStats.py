0d9e34ddfbfc6d4f40f3246118d79fdc7b9f320f
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-10-18 20:52:06 -0400

    Added sport parameter

--- backend/getPlayerStats.py
+++ backend/getPlayerStats.py
@@ -7,22 +7,25 @@ import sys
 # send_request takes in the first name and last name of the player to be found. It then returns the json for the
 # 2016-2017 regular season stats (current stats are being approved by mysportsfeed staff)
 # Needed approval as a student/developer to access current stats without paying.
-def send_request(fName, lName):
+def send_request(fName, lName, sport):
 
     playerName = fName + "-" + lName
     
+    
     try:
         response = requests.get(
-            url="https://api.mysportsfeeds.com/v1.1/pull/nhl/2016-2017-regular/cumulative_player_stats.json?playerstats=G,A,Pts,Sh",
+            url="https://api.mysportsfeeds.com/v1.1/pull/"+sport+"/2016-2017-regular/cumulative_player_stats.json?playerstats=G,A,Pts,Sh",
             params={
                     "player":playerName
                     },
             headers={"Authorization": "Basic " + base64.b64encode('{}:{}'.format("Tolton15","sportsApp123").encode('utf-8')).decode('ascii')})
-        
-        print json.dumps(response.json(), indent = 1)
+        try:
+            print json.dumps(response.json(), indent = 1)
+        except ValueError:
+            print('Decoding JSON has failed, wrong sport maybe?')
 
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
 
 
-send_request(sys.argv[1], sys.argv[2])
+send_request(sys.argv[1], sys.argv[2], sys.argv[3])
