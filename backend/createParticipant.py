665b9c86ea5f47f696ab34f3854c7677f3bd4f47
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-10-20 04:10:35 -0400

    Added salt and better duplicate username/salt checking

--- backend/createParticipant.py
+++ backend/createParticipant.py
@@ -3,7 +3,10 @@ from _mysql import Error
 import sys
 import hashlib
 import os
+import random
 
+# createParticipant creates a new user
+# It takes in the username and the password of the new user. A salt is randomly generated and the password is hashed and salted. Then the username password and salt are stored in the database.
 def createParticipant(username, password):
     
     userName = "teamOgre"
@@ -18,24 +21,32 @@ def createParticipant(username, password):
         return False
     
     cursor = db.cursor()
-
     
-    #Inserting a new participant
-    salt = os.urandom(8)
-    hash_object = hashlib.sha256(password)
+    salt = os.urandom(16).encode('hex')
+
+    # Avoiding duplicate salts(yes I know this will almost never happen.. except during the Alpha -.-)
+    cursor.execute("SELECT COUNT(username) FROM participants WHERE salt = %s", salt)
+    ret = cursor.fetchone()
+    while ret[0] > 0:
+        salt = os.urandom(16).encode('hex')
+        cursor.execute("SELECT COUNT(username) FROM participants WHERE salt = %s", salt)
+        ret = cursor.fetchone()
+
+    # Creating the hashed and salted password
+    hash_object = hashlib.sha256(salt + password)
     hex_dig = hash_object.hexdigest()
 
-    try:
-        query = "INSERT INTO participants(username, password) VALUES(%s, %s)"
-        cursor.execute(query, (username, hex_dig))
+    # Avoiding duplicate usernames(if there is 1 or more usernames the same returned Count will be > 0)
+    cursor.execute("SELECT COUNT(username) FROM participants WHERE username = %s", username)
+    ret = cursor.fetchone()
+    if ret[0] > 0:
+        print "Username is taken"
+    else:
+        cursor.execute("INSERT INTO participants(username, password, salt) VALUES(%s, %s, %s)", (username, hex_dig, salt))
         
-        db.commit()
-    except Error as h:
-        print "Duplicate entry"
+    db.commit()
     
     db.close()
-
-
 
 
 createParticipant(sys.argv[1], sys.argv[2])
