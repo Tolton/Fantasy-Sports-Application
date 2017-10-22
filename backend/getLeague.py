bbddfa9701fdbef99be1b750b06580cbe83ed508
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-10-22 01:51:28 -0400

    Adding getLeague.py

---
+++ backend/getLeague.py
@@ -0,0 +1,58 @@
+import MySQLdb
+from _mysql import Error
+import json
+import sys
+import os
+
+# getLeague takes in the username of the participant you want to get the leagues for
+# dumps the json of all leagues returned
+def getLeague(username):
+    
+    userName = "teamOgre"
+    passName = "sportsApp123"
+    hostName = "cis4250.cpnptclkba5c.ca-central-1.rds.amazonaws.com"
+    dbName   = "fantasySportsApplication"
+    
+    try:
+        db = MySQLdb.connect(user=userName, passwd=passName, host=hostName, db = dbName)
+    
+    except Error as e:
+        return False
+
+    cursor = db.cursor()
+
+    # get the participant_id
+    query = "SELECT participant_id FROM participants WHERE username = %s"
+    cursor.execute(query, username)
+    
+    participantID = cursor.fetchone()
+
+    # get the league_id
+    query = "SELECT league_id FROM league_roster WHERE participant_id = %s"
+    cursor.execute(query, participantID)
+    
+    leagueID = cursor.fetchall()
+
+    # create the json of the leagues returned
+    jsonRet = {}
+    i = 0
+    for row in leagueID:
+        query = "SELECT * FROM league WHERE league_id = %s"
+        cursor.execute(query, row[0])
+
+        jsonRet[i] = cursor.fetchone()
+        i = i + 1
+    
+    
+
+    print json.dumps(jsonRet);
+
+
+
+
+    db.close()
+
+
+
+#username
+getLeague(sys.argv[1])
