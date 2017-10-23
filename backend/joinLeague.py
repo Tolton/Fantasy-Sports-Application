33f5ddb0b975fb85d753b22987bf31dee03b0bf0
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-10-23 17:09:52 -0400

    Added checking for league password

--- backend/joinLeague.py
+++ backend/joinLeague.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 # joinLeague takes in the username and the league name to join. It then inserts this information into the league_roster table
-def joinLeague(username, leagueName):
+def joinLeague(username, leagueName, leaguePassword):
     
     userName = "teamOgre"
     passName = "sportsApp123"
@@ -22,13 +22,18 @@ def joinLeague(username, leagueName):
     cursor.execute("SELECT participant_id FROM participants WHERE username = %s", username)
     participantID = cursor.fetchone()
 
-    cursor.execute("SELECT league_id FROM league WHERE league_name = %s", leagueName)
-    leagueID = cursor.fetchone()
+    cursor.execute("SELECT league_id, league_pass FROM league WHERE league_name = %s", leagueName)
+    leagueID = cursor.fetchall()
 
-    cursor.execute("INSERT INTO league_roster(participant_id, league_id) VALUES(%s, %s)", (participantID[0], leagueID[0]))
-    db.commit()
+    if leagueID[0][1] != leaguePassword:
+        print "Invalid password"
+    else:
+        teamName = "My Team"
+        
+        cursor.execute("INSERT INTO league_roster(participant_id, league_id, team_name) VALUES(%s, %s, %s)", (participantID[0], leagueID[0][0], teamName))
+        db.commit()
     
     db.close()
 
 
-joinLeague(sys.argv[1], sys.argv[2])
+joinLeague(sys.argv[1], sys.argv[2], sys.argv[3])
