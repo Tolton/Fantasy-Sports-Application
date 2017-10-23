cb56e80b8173502651fedbe2b0ed3e013bc2aece
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-10-23 17:09:07 -0400

    Added league password

--- backend/createLeague.py
+++ backend/createLeague.py
@@ -5,7 +5,7 @@ import sys
 # createLeague takes in the username, league name, list of rules, max players for the league and the name in the 3 letter abbreviation
 # inserts into league as well as the intermediary table -> league_roster
 
-def createLeague(username, leagueName, rulesList, maxPlayers, sportID):
+def createLeague(username, leagueName, leaguePassword, rulesList, maxPlayers, sportID):
     
     userName = "teamOgre"
     passName = "sportsApp123"
@@ -25,38 +25,34 @@ def createLeague(username, leagueName, rulesList, maxPlayers, sportID):
 
     participantID = cursor.fetchone()
     
-    try:
-        # Will print League Name taken if there already exists a league name of the same name in the table
-        cursor.execute("SELECT COUNT(league_name) FROM league WHERE league_name = %s", leagueName)
-        ret = cursor.fetchone()
-        if ret[0] > 0:
-            print "League Name taken"
-        else:
     
+        # Will print League Name taken if there already exists a league name of the same name in the table
+    cursor.execute("SELECT COUNT(league_name) FROM league WHERE league_name = %s", leagueName)
+    ret = cursor.fetchone()
+    if ret[0] > 0:
+        print "League Name taken"
+    else:
+    
+        query = "INSERT INTO league(league_name, league_pass, commissioner_id, rules, max_players, sport_id) VALUES(%s, %s, %s, %s, %s, %s)"
+        cursor.execute(query, (leagueName, leaguePassword, participantID[0], rulesList, maxPlayers, sportID))
+        db.commit()
         
-            query = "INSERT INTO league(league_name, commissioner_id, rules, max_players, sport_id) VALUES(%s, %s, %s, %s, %s)"
-            cursor.execute(query, (leagueName, participantID[0], rulesList, maxPlayers, sportID))
-            db.commit()
+        query = "SELECT LAST_INSERT_ID()"
+        cursor.execute(query)
         
-            query = "SELECT LAST_INSERT_ID()"
-            cursor.execute(query)
-        
-        
-            leagueID = cursor.fetchone()
+    
+        leagueID = cursor.fetchone()
             
-            teamName = "My Team"
+        teamName = "My Team"
 
-            query = "INSERT INTO league_roster(participant_id, league_id, team_name) VALUES(%s, %s, %s)"
-            cursor.execute(query, (participantID[0], leagueID[0], teamName))
-            db.commit()
+        query = "INSERT INTO league_roster(participant_id, league_id, team_name) VALUES(%s, %s, %s)"
+        cursor.execute(query, (participantID[0], leagueID[0], teamName))
+        db.commit()
 
-    except TypeError:
-        print "Something went wrong.."
     
     
     db.close()
 
 
-
-#username, league name, rules, max players, sport id
-createLeague(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+#username, league name, league password, rules, max players, sport id
+createLeague(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
