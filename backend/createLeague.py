ea45613a7fa497a461ea9df6123b425d021f9db1
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-11-05 04:44:53 -0500

    Added private for league

--- backend/createLeague.py
+++ backend/createLeague.py
@@ -2,10 +2,10 @@ import MySQLdb
 from _mysql import Error
 import sys
 
-# createLeague takes in the username, league name, list of rules, max players for the league and the name in the 3 letter abbreviation
+# createLeague takes in the username, league name, list of rules, max players for the league and the name in the 3 letter abbreviation, as well as the team name and the private key (1 for private or 0 for public)
 # inserts into league as well as the intermediary table -> league_roster
 
-def createLeague(username, leagueName, leaguePassword, rulesList, maxPlayers, sportID, teamName):
+def createLeague(username, leagueName, leaguePassword, rulesList, maxPlayers, sportID, teamName, privateKey):
     
     userName = "teamOgre"
     passName = "sportsApp123"
@@ -36,13 +36,20 @@ def createLeague(username, leagueName, leaguePassword, rulesList, maxPlayers, sp
         print "League Name taken"
         sys.exit(0)
 
-    
-    query = "INSERT INTO league(league_name, league_pass, commissioner_id, rules, max_players, sport_id) VALUES(%s, %s, %s, %s, %s, %s)"
-    cursor.execute(query, (leagueName, leaguePassword, participantID[0], rulesList, maxPlayers, sportID))
-    db.commit()
+    # Changes the password to blank if the league is public
+    if privateKey == "0":
+        leaguePassword = ""
+    elif leaguePassword == "":
+        # If the league is private and has no password, exit
+        print "No password given for private league"
+        sys.exit(0)
 
+    query = "INSERT INTO league(league_name, league_pass, commissioner_id, rules, max_players, sport_id, private) VALUES(%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (leagueName, leaguePassword, participantID[0], rulesList, maxPlayers, sportID, privateKey))
+    db.commit()
+    
     # Grab the league id of the last instert
-    cursor.execute("SELECT league_id FROM league WHERE league_name = %s AND league_pass = %s AND commissioner_id = %s AND rules = %s AND max_players = %s AND sport_id = %s", (leagueName, leaguePassword, participantID[0], rulesList, maxPlayers, sportID))
+    cursor.execute("SELECT league_id FROM league WHERE league_name = %s AND league_pass = %s AND commissioner_id = %s AND rules = %s AND max_players = %s AND sport_id = %s AND private = %s", (leagueName, leaguePassword, participantID[0], rulesList, maxPlayers, sportID, privateKey))
         
     
     leagueID = cursor.fetchone()
@@ -57,4 +64,4 @@ def createLeague(username, leagueName, leaguePassword, rulesList, maxPlayers, sp
 
 
 #username, league name, league password, rules, max players, sport id, team name
-createLeague(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
+createLeague(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8])
