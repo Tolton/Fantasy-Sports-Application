321af19d730d98eba3297e9e3ca78208e56f1221
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-11-08 22:50:19 -0500

    Protects against joining a league with max teams

--- backend/joinLeague.py
+++ backend/joinLeague.py
@@ -33,27 +33,39 @@ def joinLeague(username, leagueName, leaguePassword, teamName):
         leaguePassword = ""
 
 # league and password checking
-    cursor.execute("SELECT league_id FROM league WHERE league_name = %s AND league_pass = %s", (leagueName, leaguePassword))
-    leagueID = cursor.fetchone()
+    cursor.execute("SELECT league_id, rules FROM league WHERE league_name = %s AND league_pass = %s", (leagueName, leaguePassword))
+    ret = cursor.fetchall()
+    leagueID = ret[0][0]
+    rulesID = ret[0][1]
+
     if cursor.rowcount == 0:
         print "Invalid league name or password."
         sys.exit(0)
+    
+# check if there already is the max teams in the league
+    cursor.execute("SELECT max_teams FROM rules WHERE rules_id = %s", [rulesID])
+    maxTeams = cursor.fetchone()
+    cursor.execute("SELECT COUNT(*) FROM league_roster WHERE league_id = %s", [leagueID])
+    leagueTeams = cursor.fetchone()
+    if leagueTeams[0] >= maxTeams[0]:
+        print "League has reached max teams."
+        sys.exit(0)
 
 # Already in a league checking
-    cursor.execute("SELECT COUNT(*) FROM league_roster WHERE participant_id = %s AND league_id = %s", (participantID[0], leagueID[0]))
+    cursor.execute("SELECT COUNT(*) FROM league_roster WHERE participant_id = %s AND league_id = %s", (participantID[0], leagueID))
     ret = cursor.fetchone()
     if ret[0] > 0:
         print "Cannot join a league you are already in."
         sys.exit(0)
 
 # team name in league already taken
-    cursor.execute("SELECT COUNT(*) FROM league_roster WHERE league_id = %s AND team_name = %s", (leagueID[0], teamName))
+    cursor.execute("SELECT COUNT(*) FROM league_roster WHERE league_id = %s AND team_name = %s", (leagueID, teamName))
     ret = cursor.fetchone()
     if ret[0] > 0:
         print "Team name already taken."
         sys.exit(0)
 
-    cursor.execute("INSERT INTO league_roster(participant_id, league_id, team_name) VALUES(%s, %s, %s)", (participantID[0], leagueID[0], teamName))
+    cursor.execute("INSERT INTO league_roster(participant_id, league_id, team_name) VALUES(%s, %s, %s)", (participantID[0], leagueID, teamName))
     db.commit()
     
     db.close()
