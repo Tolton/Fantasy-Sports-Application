b41b8013e3dd060c7f13ad399b7828f23c9bc13d
Jack Tolton <toltonj@mail.uoguelph.ca>
2017-11-29 17:58:19 -0500

    Took out max players

--- backend/createLeague.py
+++ backend/createLeague.py
@@ -10,7 +10,7 @@ import os
 # statsList is a comma delimited string cosisting of the following GamesPlayed,Goals,Assists,Points,HatTricks,Penalties,PenaltyMinutes,PowerplayGoals,PowerplayAssists,PowerplayPoints,ShorthandedGoals,ShorthandedAssists,ShorthandedPoints,GameWinningGoals,GameTyingGoals,PlusMinus,Shots,ShotPercentage,Hits,Faceoffs,FaceoffWins,FaceoffLosses,FaceoffPercent,Wins,Losses,OvertimeWins,OvertimeLosses,GoalsAgainst,ShotsAgainst,Saves,GoalsAgainstAverage,SavePercentage,Shutouts,GamesStarted,CreditForGame,MinutesPlayed
 # inserts into league as well as the intermediary table -> league_roster and inserts the rules into the rules table.
 
-def createLeague(username, leagueName, leaguePassword, maxPlayers, sportID, teamName, privateKey, maxTeams, maxC, maxLW, maxRW, maxD, maxG, statsList):
+def createLeague(username, leagueName, leaguePassword, maxTeams, sportID, teamName, privateKey, maxC, maxLW, maxRW, maxD, maxG, statsList):
     
     userName = "teamOgre"
     passName = "sportsApp123"
@@ -53,7 +53,7 @@ def createLeague(username, leagueName, leaguePassword, maxPlayers, sportID, team
         sys.exit(0)
 
     # Insert all the values into the rules table
-    cursor.execute("INSERT INTO rules(max_teams, max_c, max_lw, max_rw, max_d, max_g, stats_list) VALUES(%s, %s, %s, %s, %s, %s, %s)", (maxTeams, maxC, maxLW, maxRW, maxD, maxG, statsList))
+    cursor.execute("INSERT INTO rules(max_c, max_lw, max_rw, max_d, max_g, stats_list) VALUES(%s, %s, %s, %s, %s, %s)", (maxC, maxLW, maxRW, maxD, maxG, statsList))
     db.commit()
     # Grab the id of the rules table insertion
     cursor.execute("SELECT LAST_INSERT_ID()")
@@ -75,8 +75,8 @@ def createLeague(username, leagueName, leaguePassword, maxPlayers, sportID, team
         hash_object = hashlib.sha256(salt + leaguePassword)
         hex_dig = hash_object.hexdigest()
 
-    query = "INSERT INTO league(league_name, league_pass, commissioner_id, rules, max_players, sport_id, private, salt) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
-    cursor.execute(query, (leagueName, hex_dig, participantID[0], rulesID[0], maxPlayers, sportID, privateKey, salt))
+    query = "INSERT INTO league(league_name, league_pass, commissioner_id, rules, max_teams, sport_id, private, salt) VALUES(%s, %s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute(query, (leagueName, hex_dig, participantID[0], rulesID[0], maxTeams, sportID, privateKey, salt))
     db.commit()
     
     # Grab the league id of the last insert
@@ -95,4 +95,4 @@ def createLeague(username, leagueName, leaguePassword, maxPlayers, sportID, team
 
 
 #username, league name, league password, rules, max players, sport id, team name
-createLeague(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11], sys.argv[12], sys.argv[13], sys.argv[14])
+createLeague(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9], sys.argv[10], sys.argv[11], sys.argv[12], sys.argv[13])
